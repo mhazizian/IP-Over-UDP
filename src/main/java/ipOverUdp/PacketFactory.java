72f227ec1b06a3085eafa0042106f1550582648d
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2019-06-19 16:56:37 +0430

    Add packetFactory class for generating and parsing Packet header fields

---
+++ src/main/java/ipOverUdp/PacketFactory.java
@@ -0,0 +1,21 @@
+package ipOverUdp;
+
+public class PacketGenerator {
+    public PacketGenerator() {
+
+    }
+
+    void setPayload(byte[] data, int size) {
+
+    }
+
+    void setSrcIp(byte[] ip) {
+
+    }
+
+    void setDstIp(byte[] ip) {
+
+    }
+
+    void set
+}
